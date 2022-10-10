17e2d25a40b8f391925c97fbfc9fcdda68cec09e
Sekhar1p <hackersekhar359@gmail.com>
2022-10-10 17:40:38 +0530

    added the day1 problem

---
+++ day1/problem2.js
@@ -0,0 +1,6 @@
+let name = "Sudhansu Sekhar Behera";
+console.log(name);
+let father = "Lima Behera";
+console.log(father);
+let mother = "Jharana Behera";
+console.log(mother);
---
+++ day1/problem3.js
@@ -0,0 +1,4 @@
+let name = "Sudhansu Sekhar Behera";
+let age = 21;
+console.log(name);
+console.log(age);
