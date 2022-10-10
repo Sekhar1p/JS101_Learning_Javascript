92e2a8306c2b85eff6e0d36f0f2b48aab03da713
Sekhar1p <hackersekhar359@gmail.com>
2022-10-10 18:07:39 +0530

    added day3 problem

---
+++ day3/problem4.js
@@ -0,0 +1,13 @@
+
+ // Given 3 numbers (all different values), print which is greatest
+let a = 5;
+let b = 10;
+let c = 15;
+
+if(a>b&&a>c){
+  console.log(a,"is greatest");
+}else if(b>a&&b>c){
+  console.log(b,"is greatest");
+}else{
+  console.log(c,"is greatest");
+}
